ab9d14a6e7a46406d6f269a6b7873ce9bff1297f
iamayushdas <ayush24das@gmail.com>
2020-04-13 10:43:42 +0530

    conditional statements

--- helloscript.sh
+++ helloscript.sh
@@ -2,13 +2,15 @@
 
 #! /usr/bin/bash
 
+#! /usr/bin/bash
+
 #hereDocDelimeter
 : 'cat << Kreativ
 this is hello creative text add
 another line 
 Kreativ '
 
-count=10
+: 'count=10
 
 if (( $count != 9 ))
 then  
@@ -20,5 +22,39 @@ then
 else   
        echo "the condition is false"
 
+fi '
+
+: 'age=10
+
+if [ "$age" -gt 9 ] && [ "$age" -lt 40 ]
+then  
+	echo "Age is correct"
+else 
+	echo "Age is not correct"
+fi '
+
+#another ways of putting square brackets
+
+: 'age=10 
+
+if [[ "$age" -gt 9  &&  "$age" -lt 40 ]]
+then  
+	echo "Age is correct"
+else 
+	echo "Age is not correct"
+fi '
+
+age=10 #using -a
+
+if [ "$age" -gt 9  -a  "$age" -lt 40 ]
+then  
+	echo "Age is correct"
+else 
+	echo "Age is not correct"
 fi
+
+
+
+
+
 
