375a5454ab5feb2582083111bc676a24113f060f
iamayushdas <ayush24das@gmail.com>
2020-04-13 13:46:56 +0530

    while loop added

--- helloscript.sh
+++ helloscript.sh
@@ -2,8 +2,6 @@
 
 #! /usr/bin/bash
 
-#! /usr/bin/bash
-
 #hereDocDelimeter
 : 'cat << Kreativ
 this is hello creative text add
@@ -44,17 +42,40 @@ else
 	echo "Age is not correct"
 fi '
 
-age=10 #using -a
+: 'age=10 #using -a
 
 if [ "$age" -gt 9  -a  "$age" -lt 40 ]
 then  
 	echo "Age is correct"
 else 
 	echo "Age is not correct"
-fi
+fi '
 
+#case statements
 
+#: 'car=HONDA
 
+#case $car in
+#	"BMW" )
+#		echo "It,s BMW" ;;
+#	"MERCEDES" )
+#		echo "It's mercedes" ;;
+#	"TOYOTA" )
+#		echo "It's toyota" ;;
+#	"HONDA" )
+#		echo "It's honda" ;;
+#	* )
+#		echo "unknown carname" ;;
+#esac '
+
+#WHILE LOOP
+
+number=1
+while [ $number -lt 10 ]
+do
+	echo "$number"
+	number=$(( number+1 ))
+done
 
 
 
